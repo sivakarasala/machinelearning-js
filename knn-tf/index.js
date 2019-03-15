f3173ef8ad220c63e6ff822ff6c3072f503b6a60
shambho <sivakrishna.karasala@eku.edu>
2019-03-15 08:14:39 -0400

    feature massage

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -3,9 +3,13 @@ const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
   return (
     features
-      .sub(predictionPoint)
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
       .pow(2)
       .sum(1)
       .pow(0.5)
@@ -24,7 +28,7 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ["lat", "long"],
+    dataColumns: ["lat", "long", "sqft_living", "sqft_lot"],
     labelColumns: ["price"]
   }
 );
